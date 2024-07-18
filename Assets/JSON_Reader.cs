4c4f5fec31138337eaa286616de7eed7786cad8d
AkiraWatanabeNeec <127703315+AkiraWatanabeNeec@users.noreply.github.com>
2024-07-18 11:14:37 +0900

    一応の完成

--- Assets/JSON_Reader.cs
+++ Assets/JSON_Reader.cs
@@ -2,9 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class JsonScene
 {
     public string name;
+    public Object[] objects;
+}
+[System.Serializable]
+public class Object
+{
+    public string type;
+    public string name;
+    public Transform transform;
+}
+[System.Serializable]
+public class Transform
+{
+    public float[] translation;
+    public float[] rotation;
+    public float[] scaling;
 }
 
 public class JSON_Reader : MonoBehaviour
@@ -12,16 +28,35 @@ public class JSON_Reader : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // 入力データパス名「 Assets/Resource/out.json 」
+        // 入力データパス名「 Assets/Resources/out.json 」
         string json_string = Resources.Load<TextAsset>("out").ToString();
-        JsonScene jsonScene = JsonUtility.FromJson<JsonScene>(json_string);
+        JsonScene jsonScene = new JsonScene();
+
+        JsonUtility.FromJsonOverwrite(json_string, jsonScene);
         Debug.Log(jsonScene.name);
-        // Debug.Log(json_string);
-    }
+        
+        foreach(Object one in jsonScene.objects)
+        {
+            float[] t = one.transform.translation;
+            Vector3 position = new Vector3(t[0], t[1], t[2]);
+
+            float[] r = one.transform.rotation;
+            Vector3 rotation = new Vector3(r[0], r[1], r[2]);
+
+            float[] s = one.transform.scaling;
+            Vector3 scaling = new Vector3(s[0], s[1], s[2]);
+
+            Debug.Log($"type : {one.type}");
+            Debug.Log($"name : {one.name}");
+            Debug.Log( "<transform>");
+            Debug.Log($" translation : {position}");
+            Debug.Log($" rotation    : {rotation}");
+            Debug.Log($" scaling     : {scaling}");
+		}
+	}
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
